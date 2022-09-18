24c8bcef05eabaf6a936c8e1bfc023a7818e9310
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2022-09-18 02:31:26 +0100

    updated

--- scripts/src.js
+++ scripts/src.js
@@ -2,8 +2,8 @@
 function sendEmail(){
   Email.send({
     Host : "smtp.elasticemail.com",
-    Username : "pelumiteejay@elasticemail.com",
-    Password : "A38D49C1D3F318BBBE2443DDE21CC188FB47",
+    Username : "pelumiolaiya1403@gmail.com",
+    Password : "629A6BD1E261C3384AB669807290B66F411D",
     To : 'pelumiolaiya1403@gmail.com',
     From :  "pelumiolaiya1403@gmail.com",
     Subject : "Message From My Portfolio Website",
