b83ebee1088323e8fe622860a406c49517e9141c
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2022-09-18 05:18:12 +0100

    updated

--- scripts/src.js
+++ scripts/src.js
@@ -2,10 +2,10 @@
 function sendEmail(){
   Email.send({
     Host : "smtp.elasticemail.com",
-    Username : "pelumijuwonteejay@gmail.com",
-    Password : "E09108C03DAE7C34BCA66CCE63E7617720B0",
+    Username : "pelumiolaiya01@gmail.com",
+    Password : "50F842E32430F2246048CD85B2EA106ABDA7",
     To : 'teqbaddie@gmail.com',
-    From :  "pelumijuwonteejay@gmail.com",
+    From :  "pelumiolaiya01@gmail.com",
     Subject : "Message From My Portfolio Website",
     Body : "Name: " + document.getElementById("name").value
                + " <br> Email: " + document.getElementById("email").value
@@ -15,3 +15,11 @@ function sendEmail(){
   message => alert("Your message has been sent successfully, I will reach out to you soon!")
 );
     }
+
+
+
+    // Host : "smtp.elasticemail.com",
+    // Username : "pelumijuwonteejay@gmail.com",
+    // Password : "7269E31C54FEA1D45C4D12CDC5EAEDA7BA34",
+    // To : 'teqbaddie@gmail.com',
+    // From :  "pelumijuwonteejay@gmail.com",
